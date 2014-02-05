9be3d285a2e606068a7af58dd4ce8cfcec6fb234
nidol <nidol@asynctive.com>
2014-02-04 21:11:30 -0400

    Added input validation, crash fix to hangman

--- Hangman.py
+++ Hangman.py
@@ -2,7 +2,7 @@
 
 """
 A simple game of hangman
-v1.0
+v1.0.1
 """
 
 import math
@@ -10,13 +10,14 @@ import random
 
 TOTAL_WRONG_GUESSES = 6
 WORDS_FILENAME = "words.txt"
+VALID_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 def getWordList(filename):
 	"""
 	Loads and returns a list of words from a file
 	"""
 	f = open(filename, "r")
-	results = f.read().strip().split('\n')
+	results = f.read().strip().upper().split('\n')
 	return results
 
 def getRandomWord(words):
@@ -37,6 +38,13 @@ def getProgress(word, guessed_chars):
 			match += "_"
 	return match
 	
+def getCharacter():
+	data = raw_input("Enter a character: ").upper()
+	if len(data) == 0 or VALID_CHARACTERS.find(data[0]) == -1:
+		return ""
+		
+	return data[0]
+		
 words = getWordList(WORDS_FILENAME)
 current_word = getRandomWord(words)
 guessed_correct = ""
@@ -48,12 +56,17 @@ while len(guessed_wrong) < TOTAL_WRONG_GUESSES:
 	if (result == current_word):
 		break;	# game ends
 	else:
+		print "You have " + str(TOTAL_WRONG_GUESSES - len(guessed_wrong)) + " guesses left"
 		temp = ""
 		for i in result:
 			temp += i + " "
 		print temp
 		
-	char = raw_input("Enter a character: ")[0].upper()
+	char = getCharacter()
+	if char == "":
+		print "Invalid character"
+		continue	# loop unchanged
+	
 	if guessed_correct.find(char) > -1 or guessed_wrong.find(char) > -1:
 		print "You already guessed that character!"
 		continue	# loop unchanged
@@ -66,3 +79,5 @@ if getProgress(current_word, guessed_correct) == current_word:
 	print "You win! The word was: " + current_word
 else:
 	print "Game Over. The word was: " + current_word
+	
+# EOF
